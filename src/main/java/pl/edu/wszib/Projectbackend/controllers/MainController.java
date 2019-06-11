508b47ecde968255f7a64fa2cc64263b7ef2022e
magdalena996 <madzik996@gmail.com>
2019-06-11 22:41:53 +0200

    Create data base model (applications).

--- src/main/java/pl/edu/wszib/Projectbackend/controllers/MainController.java
+++ src/main/java/pl/edu/wszib/Projectbackend/controllers/MainController.java
@@ -1,4 +1,13 @@
 package pl.edu.wszib.Projectbackend.controllers;
 
-public class MainController {
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class MainController
+{
+
 }
---
+++ src/main/java/pl/edu/wszib/Projectbackend/dbmodels/Application.java
@@ -0,0 +1,41 @@
+package pl.edu.wszib.Projectbackend.dbmodels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="applications")
+public class Application
+{
+
+    @GeneratedValue
+    @Id
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String nick;
+    @Column(nullable = false)
+    private int age;
+    @Column(nullable = false)
+    private String position;
+    @Column(nullable = false)
+    private int knowledgeTS3;
+    @Column(nullable = false)
+    private int ranks;
+    @Column(nullable = false)
+    private int createChannels;
+    @Column(nullable = false)
+    private int groupWork;
+    @Column(nullable = false)
+    private String wordsAboutSelf;
+    @Column(nullable = false)
+    private String whyApply;
+}
